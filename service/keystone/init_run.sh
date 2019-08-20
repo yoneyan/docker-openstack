89c9dbe1cc932f633a684517e56de25527d23af3
yoneyan <github.yoneyan.net>
2019-08-21 01:49:34 +0900

    move keystone and horizon and glance

---
+++ service/keystone/init_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+openstack-config --set /etc/keystone/keystone.conf database connection ${CONNECTION-mysql://root:root@172.16.100.231/keystone}
+su keystone -s /bin/sh -c "keystone-manage db_sync"
+
+keystone-manage bootstrap --bootstrap-password ADMIN_PASS \
+  --bootstrap-admin-url http://controller:5000/v3/ \
+  --bootstrap-internal-url http://controller:5000/v3/ \
+  --bootstrap-public-url http://controller:5000/v3/ \
+  --bootstrap-region-id RegionOne
+
+ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
+
