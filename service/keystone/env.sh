b8e992e2a6618010fa8c95bdefe22e539bb8c9ef
yoneyan <github@yoneyan.net>
2019-09-14 02:52:09 +0900

    modify keystone dockerfile and config

---
+++ service/keystone/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod 600 /keystonerc
+source /keystonerc
+echo "source /keystonerc " >> ~/.bash_profile
--- service/keystone/init.sh
+++ service/keystone/init.sh
@@ -4,9 +4,9 @@ su -s /bin/bash keystone -c "keystone-manage db_sync"
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 
-export controller=10.30.0.2
-keystone-manage bootstrap --bootstrap-password ADMIN_PASS \
-  --bootstrap-admin-url http://controller:5000/v3/ \
-  --bootstrap-internal-url http://controller:5000/v3/ \
-  --bootstrap-public-url http://controller:5000/v3/ \
+#export controller=10.30.0.2
+keystone-manage bootstrap --bootstrap-password adminpassword \
+  --bootstrap-admin-url http://10.30.0.2:5000/v3/ \
+  --bootstrap-internal-url http://10.30.0.2:5000/v3/ \
+  --bootstrap-public-url http://10.30.0.2:5000/v3/ \
   --bootstrap-region-id RegionOne
